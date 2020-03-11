c9cae18d35cfd53d4c74b6a9e0d93a30e0e233b6
pmontalb <f.paolo.montalbano@gmail.com>
2020-03-11 16:30:54 +0000

    first prototype for sanitizer flags

---
+++ TsanFlags.cmake
@@ -0,0 +1,11 @@
+include_guard()
+
+if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+    set(CMAKE_CXX_FLAGS_TSAN "-ggdb3 -O0 -fsanitize=thread" CACHE STRING "" FORCE)
+elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
+    set(CMAKE_CXX_FLAGS_TSAN "-ggdb3 -O0 -fsanitize=thread" CACHE STRING "" FORCE)
+elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Intel")
+    message(FATAL "icc not supported")
+elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
+    message(FATAL "MSVC not supported")
+endif()
---
+++ UbsanFlags.cmake
@@ -0,0 +1,11 @@
+include_guard()
+
+if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+    set(CMAKE_CXX_FLAGS_UBSAN "-ggdb3 -O0 -fsanitize=undefined -fsanitize=integer -fsanitize=nullability" CACHE STRING "" FORCE)
+elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
+    set(CMAKE_CXX_FLAGS_UBSAN "-ggdb3 -O0 -fsanitize=undefined -fsanitize=float-divide-by-zero -fsanitize=float-cast-overflow" CACHE STRING "" FORCE)
+elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Intel")
+    message(FATAL "icc not supported")
+elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
+    message(FATAL "MSVC not supported")
+endif()
