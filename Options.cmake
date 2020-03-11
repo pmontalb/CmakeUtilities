9c6043c92b6f947e9de6023c067a76bd3686e06b
pmontalb <f.paolo.montalbano@gmail.com>
2020-03-11 15:32:31 +0000

    arch flags

--- Options.cmake
+++ Options.cmake
@@ -18,5 +18,7 @@ option(OPTIMIZATIONS_USE_OFAST "Use -Ofast in release mode instead of O3" OFF)
 option(OPTIMIZATIONS_USE_MEMORY_OPTIMIZATION "Use Graphite/Polly in release mode" OFF)
 option(OPTIMIZATIONS_USE_FAST_MATH "Use -ffast-math in release mode" OFF)
 option(OPTIMIZATIONS_USE_LTO "Use Link-Time Optimization in release mode" OFF)
+option(OPTIMIZATIONS_USE_NATIVE_ARCHITECTURE "Use -march=native" ON)
+option(OPTIMIZATIONS_USE_NATIVE_TUNING "Use -mtune=native" OFF)
 
-option(BUILD_SYSTEM_USE_CCACHE ON)
+option(BUILD_SYSTEM_USE_CCACHE "Use ccache" ON)
