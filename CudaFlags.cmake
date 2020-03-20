0d77f84dd8f4d15a1d0df9d1f9ec7d42c4df0810
pmontalb <f.paolo.montalbano@gmail.com>
2020-03-20 14:20:29 +0000

    Automatic CUDA GPU detection

--- CudaFlags.cmake
+++ CudaFlags.cmake
@@ -1,6 +1,28 @@
 include_guard()
 
-if (LANGUAGES_USE_CUDA)
+# /* https://stackoverflow.com/questions/2285185/easiest-way-to-test-for-existence-of-cuda-capable-gpu-from-cmake */
+find_package(CUDA)
+if(CUDA_FOUND)
+    try_run(RUN_RESULT_VAR COMPILE_RESULT_VAR
+            ${CMAKE_BINARY_DIR}
+            ${CMAKE_CURRENT_SOURCE_DIR}/cmake/Utilities/HasCudaGpu.c
+            CMAKE_FLAGS
+            -DINCLUDE_DIRECTORIES:STRING=${CUDA_TOOLKIT_INCLUDE}
+            -DLINK_LIBRARIES:STRING=${CUDA_CUDART_LIBRARY}
+            COMPILE_OUTPUT_VARIABLE COMPILE_OUTPUT_VAR
+            RUN_OUTPUT_VARIABLE RUN_OUTPUT_VAR)
+    message(STATUS "${RUN_OUTPUT_VAR}") # Display number of GPUs found
+    # COMPILE_RESULT_VAR is TRUE when compile succeeds
+    # RUN_RESULT_VAR is zero when a GPU is found
+    if(COMPILE_RESULT_VAR AND NOT RUN_RESULT_VAR)
+        set(CONFIG_FOUND_CUDA_GPU TRUE CACHE BOOL "Whether CUDA-capable GPU is present")
+    else()
+        set(CONFIG_FOUND_CUDA_GPU FALSE CACHE BOOL "Whether CUDA-capable GPU is present")
+    endif()
+    mark_as_advanced(CONFIG_FOUND_CUDA_GPU)
+endif(CUDA_FOUND)
+
+if (CONFIG_FOUND_CUDA_GPU AND LANGUAGES_USE_CUDA)
     set(PATHS_CUDA_PATH "/usr/local")
     mark_as_advanced(PATHS_CUDA_PATH)
 
