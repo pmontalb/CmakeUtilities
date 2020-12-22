13f500ede20896dd82b2c26baf0e4e9772157425
pmontalb <f.paolo.montalbano@gmail.com>
2020-12-23 00:59:13 +0100

    activate STL debug flags for every non-release build and put it in the right place - gtest was crashing

--- Flags.cmake
+++ Flags.cmake
@@ -1,15 +1,5 @@
 include_guard()
 
-include(cmake/Options.cmake)
-include(cmake/Configurations.cmake)
-include(cmake/Warnings.cmake)
-include(cmake/ArchitectureFlags.cmake)
-include(cmake/OptimizationFlags.cmake)
-include(cmake/DebugFlags.cmake)
-include(cmake/SanitizerFlags.cmake)
-include(cmake/CodeCoverageFlags.cmake)
-include(cmake/IncludeWhatYouUse.cmake)
-
 if (NOT FLAGS_CPP_STANDARD)
     message(STATUS "Using C++14 standard as nothing specified")
     set(FLAGS_CPP_STANDARD 14)
@@ -19,9 +9,19 @@ endif()
 set(CMAKE_CXX_STANDARD ${FLAGS_CPP_STANDARD})
 
 set(STL_DEBUG_FLAGS "")
-if (CMAKE_BUILD_TYPE STREQUAL "Debug" AND DEBUG_USE_STL_DEBUG)
+if ((NOT CMAKE_BUILD_TYPE STREQUAL "Release") AND DEBUG_USE_STL_DEBUG)
     message(STATUS "Use STL debug")
-    set(STL_DEBUG_FLAGS -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC)
+    set(STL_DEBUG_FLAGS "-D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC")
 endif()
+
+include(cmake/Options.cmake)
+include(cmake/Configurations.cmake)
+include(cmake/Warnings.cmake)
+include(cmake/ArchitectureFlags.cmake)
+include(cmake/OptimizationFlags.cmake)
+include(cmake/DebugFlags.cmake)
+include(cmake/SanitizerFlags.cmake)
+include(cmake/CodeCoverageFlags.cmake)
+include(cmake/IncludeWhatYouUse.cmake)
 
 
