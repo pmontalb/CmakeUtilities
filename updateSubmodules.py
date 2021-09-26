2a54d70c082d3576abed2348a2c1053657d63701
pmontalb <f.paolo.montalbano@gmail.com>
2021-09-26 20:07:05 +0200

    removed git init submodules

--- updateSubmodules.py
+++ updateSubmodules.py
@@ -23,6 +23,7 @@ def update_directory(directory):
     files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
     files = [os.path.join(directory, f) for f in files if f.endswith(".gitmodules")]
     if len(files) == 0:
+        print("There are no gitmodules in dir[{}]".format(directory))
         return
     if len(files) > 1:
         raise ValueError()
@@ -37,12 +38,12 @@ def update_directory(directory):
                 raise ValueError()
             modules.append(tokens[1])
 
-    output = subprocess.check_output(["git", "submodule", "init"])
-    print("Initializing submodules... \n\t[{}]".format(output))
-
-    output = subprocess.check_output(["git", "submodule", "update", "--init", "--recursive"])
-    print("Initializing submodules recursively... \n\t[{}]".format(output))
-    print()
+    # output = subprocess.check_output(["git", "submodule", "init"])
+    # print("Initializing submodules... \n\t[{}]".format(output))
+    #
+    # output = subprocess.check_output(["git", "submodule", "update", "--init", "--recursive"])
+    # print("Initializing submodules recursively... \n\t[{}]".format(output))
+    # print()
 
     for module in modules:
         module_dir = os.path.join(directory, module)
