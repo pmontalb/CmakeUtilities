c98ef21560a58c8cf42d7e122adce3d205084e0f
pmontalb <f.paolo.montalbano@gmail.com>
2022-10-20 19:56:12 +0200

    updating new cmake functionalities for cuda

--- CudaFlags.cmake
+++ CudaFlags.cmake
@@ -1,7 +1,7 @@
 include_guard()
 
 # /* https://stackoverflow.com/questions/2285185/easiest-way-to-test-for-existence-of-cuda-capable-gpu-from-cmake */
-find_package(CUDA)
+find_package(CUDA REQUIRED)
 if (CUDA_FOUND)
 	try_run(RUN_RESULT_VAR COMPILE_RESULT_VAR
 			${CMAKE_BINARY_DIR}
@@ -23,19 +23,17 @@ if (CUDA_FOUND)
 endif (CUDA_FOUND)
 
 if (CONFIG_FOUND_CUDA_GPU AND LANGUAGES_USE_CUDA)
-	set(PATHS_CUDA_PATH "/usr/local")
-	mark_as_advanced(PATHS_CUDA_PATH)
-
-	if (NOT LANGUAGES_CUDA_VERSION)
-		message(STATUS "Using CUDA 11.4 as nothing specified")
-		set(LANGUAGES_CUDA_VERSION 11.4)
-	else ()
-		message(STATUS "Using CUDA ${LANGUAGES_CUDA_VERSION}")
-	endif ()
-
 	set(CMAKE_CUDA_STANDARD ${FLAGS_CPP_STANDARD})
 	set(CMAKE_CUDA_STANDARD_REQUIRED ON)
-	set(CMAKE_CUDA_COMPILER /usr/local/cuda-${LANGUAGES_CUDA_VERSION}/bin/nvcc CACHE PATH "" FORCE)
+
+	message(STATUS "CUDA version: ${CUDA_VERSION}")
+
+	if (NOT CMAKE_CUDA_COMPILER)
+		set(CMAKE_CUDA_COMPILER "/usr/local/cuda/bin/nvcc")
+	endif()
+
+	cuda_select_nvcc_arch_flags(ARCH_FLAGS Auto)
+	message(STATUS "CUDA architecture flags: ${ARCH_FLAGS}")
 
 	if (NOT LANGUAGES_CUDA_ARCHITECTURE)
 		message(STATUS "Using gen 61 as nothing specified")
@@ -43,9 +41,10 @@ if (CONFIG_FOUND_CUDA_GPU AND LANGUAGES_USE_CUDA)
 	else ()
 		message(STATUS "Using gen ${LANGUAGES_CUDA_ARCHITECTURE}")
 	endif ()
+	message("CUDA Compiler: ${CMAKE_CUDA_COMPILER_ID}")
 	set(CMAKE_CUDA_ARCHITECTURES ${LANGUAGES_CUDA_ARCHITECTURE})
 
-	set(DEFAULT_CUDA_FLAGS -restrict -Xcompiler -Wno-unknown-pragmas --default-stream per-thread)
+	set(DEFAULT_CUDA_FLAGS ${ARCH_FLAGS} -restrict -Xcompiler -Wno-unknown-pragmas --default-stream per-thread)
 	if (LANGUAGES_USE_64_BITS)
 		set(DEFAULT_CUDA_FLAGS ${DEFAULT_CUDA_FLAGS} -m64)
 	endif ()
